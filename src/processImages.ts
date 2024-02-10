07ee83fcf54cc984728e3d5ae86333db6b1804d8
Chris Hughes <naokotani@protonmail.com>
2024-02-10 02:30:17 -0400

    improved logic and flow of processImage module

--- src/processImages.ts
+++ src/processImages.ts
@@ -1,14 +1,19 @@
 import { watch } from "fs";
 import sharp from "sharp";
 import path from "path";
-import { readdir } from "node:fs/promises";
+import fs from "node:fs/promises";
 const imagesPath = path.join(__dirname, "../static/images");
 import { access, constants } from "node:fs/promises";
 
 const processImages = async () => {
-  const watcher = watch(imagesPath, (event, filename) => {
-    console.log(`${event} detected in /static/images`);
-    filename && checkAccess(filename);
+  const watcher = watch(imagesPath, (_, filename) => {
+    if (
+      filename &&
+      !filename.startsWith("thumb-") &&
+      (filename.endsWith(".png") || filename.endsWith(".jpg"))
+    ) {
+      checkAccess(filename);
+    }
   });
 
   process.on("SIGINT", () => {
@@ -19,34 +24,37 @@ const processImages = async () => {
 };
 
 async function checkAccess(filename: string) {
-  const images = await readdir(imagesPath);
-
-  if (images.includes(`thumb-${filename}`) || filename.startsWith("thumb-")) {
-    return;
-  }
-
   try {
     await access(`${imagesPath}/${filename}`, constants.R_OK | constants.W_OK);
-    transformImage(filename);
+    resizeImage(filename);
   } catch {
     return;
   }
 }
 
-async function transformImage(filename: string) {
-  if ((filename.endsWith(".png") || filename.endsWith(".jpg"))) {
+async function resizeImage(filename: string) {
+  const images = await fs.readdir(imagesPath);
+  if (images.includes(`thumb-${filename}`)) {
     try {
-      console.log(`Processing image ${filename}`);
-      sharp(`${imagesPath}/${filename}`)
-        .resize(384, 185, {
-          fit: "cover",
-        })
-        .toFile(`${imagesPath}/thumb-${filename}`);
-      console.log(`${filename} resized successfully`);
+      await fs.rm(`${imagesPath}/thumb-${filename}`);
+      console.log(`Deleted old thumb for ${filename}`);
     } catch (e) {
-      console.error("File processing failed" + e);
+      console.error("Error deleting old thumb: " + e);
     }
   }
+
+  try {
+    console.log(`Processing image ${filename}`);
+    sharp(`${imagesPath}/${filename}`)
+      .resize(384, 185, {
+        fit: "cover",
+      })
+      .toFile(`${imagesPath}/thumb-${filename}`);
+  } catch (e) {
+    console.error("File processing failed" + e);
+    return;
+  }
+  console.log(`${filename} resized successfully`);
 }
 
 export default processImages;
