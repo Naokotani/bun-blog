de7107eb615d5da95707af4d5be109cfb5a29317
Chris Hughes <naokotani@protonmail.com>
2024-02-10 00:04:01 -0400

    improved the control flow of processImage
    I improved the control flow of process image so that it checks to make
    sure that a thumb image does not already exist and then it ensures
    that it has access to the file in question to do the transform

--- src/processImages.ts
+++ src/processImages.ts
@@ -1,27 +1,14 @@
 import { watch } from "fs";
 import sharp from "sharp";
 import path from "path";
+import { readdir } from "node:fs/promises";
 const imagesPath = path.join(__dirname, "../static/images");
+import { access, constants } from "node:fs/promises";
 
-const processImages = () => {
+const processImages = async () => {
   const watcher = watch(imagesPath, (event, filename) => {
-    if (
-      filename &&
-      !filename.startsWith("thumb-") &&
-      (filename.endsWith(".png") || filename.endsWith(".jpg"))
-    ) {
-      console.log(`${event} detected in /static/images`);
-      console.log(`Processing image ${filename}`);
-      try {
-        sharp(`${imagesPath}/${filename}`)
-          .resize(384, 185, {
-            fit: "cover",
-          })
-          .toFile(`${imagesPath}/thumb-${filename}`);
-      } catch (e) {
-        console.error("File processing failed" + e);
-      }
-    }
+    console.log(`${event} detected in /static/images`);
+    filename && checkAccess(filename);
   });
 
   process.on("SIGINT", () => {
@@ -30,5 +17,36 @@ const processImages = () => {
     process.exit(0);
   });
 };
+
+async function checkAccess(filename: string) {
+  const images = await readdir(imagesPath);
+
+  if (images.includes(`thumb-${filename}`) || filename.startsWith("thumb-")) {
+    return;
+  }
+
+  try {
+    await access(`${imagesPath}/${filename}`, constants.R_OK | constants.W_OK);
+    transformImage(filename);
+  } catch {
+    return;
+  }
+}
+
+async function transformImage(filename: string) {
+  if ((filename.endsWith(".png") || filename.endsWith(".jpg"))) {
+    try {
+      console.log(`Processing image ${filename}`);
+      sharp(`${imagesPath}/${filename}`)
+        .resize(384, 185, {
+          fit: "cover",
+        })
+        .toFile(`${imagesPath}/thumb-${filename}`);
+      console.log(`${filename} resized successfully`);
+    } catch (e) {
+      console.error("File processing failed" + e);
+    }
+  }
+}
 
 export default processImages;
